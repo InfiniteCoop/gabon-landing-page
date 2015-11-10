45f08e65ec7cbccc02df30d2d5ba1ee0bca4f3d9
Cooper <cooperthomascooper@gmail.com>
2015-11-10 10:53:00 -0500

    First commit

---
+++ js/gab.js
@@ -0,0 +1,96 @@
+   var lang = {
+    en: {
+        EngButton: "English",
+        FrButton: "Français",
+        Title: "National Land Use Plan",
+        SubTitle: "The interactive platform",
+        Module1: "Land Use",
+        Module2: "Planning",
+        Module3: "Documentation",
+        AboutTitle: "About the Plan",
+        AboutText: "<p>The Interactive forest atlas of Gabon is a living, dynamic forest monitoring system that provides unbiased and up-to-date information on Gabon's forest sector. Built on a geographic information system (GIS) platform, the Atlas aims to strengthen forest management and land use planning by bringing information on all major land use categories onto the same standardized platform.</p><p>The underlying forest atlas database is supported and kept up-to-date by the Ministry of Forest, Environment, and Natural Resource Protection and the World Resources Institute (WRI), releasing new information as it becomes available via this <a class='bodyLink' href='http://www.gab.forest-atlas.org/map'>mapping application</a>. Other publications are released periodically and can be found in the <a class='bodyLink' href='http://gab-data.forest-atlas.org'>download section</a>.</p><p>The Interactive forest atlas of Gabon overcomes a key data challenge by integrating forest management classes with forest cover extent and change data from <a class='bodyLink' href='http://www.globalforestwatch.org/'>Global Forest Watch’s</a> near-real-time monitoring system. With a simple click, users can run complex analyses and explore the most up-to-date land cover changes within a variety of land allocations. To learn about the wide range of functionalities, visit the online tutorial.</p><p>Unless otherwise noted, Atlas data are licensed under a <a class='bodyLink' href='http://creativecommons.org/licenses/by/4.0/'>Creative Commons Attribution 4.0 International License</a>. You are free to copy and redistribute the material in any medium or format, and to transform and build upon the material for any purpose, even commercially. You must give appropriate credit, provide a link to the license, and indicate if changes were made. When displaying and citing the data, use the appropriate credit as listed for the corresponding dataset in the <a class='bodyLink' href='http://gab-data.forest-atlas.org'>download section</a>.</p>",
+        AppTitle: "Other applications",
+        PartnerTitle: "A partnership between",
+        Share: "Share this page",
+        ContactUs: "Contact us"
+    },
+    
+    fr: {
+        EngButton: "English",
+        FrButton: "Français",
+        Title: "Plan National d'Affectation des Terres",
+        SubTitle: "La plate-forme interactive",
+        Module1: "Affectation actuelle",
+        Module2: "Planification",
+        Module3: "Documentation",
+        AboutTitle: "A Propos le Plan",
+        AboutText: "<p>Le PNAT est un processus de planification par lequel le gouvernement établit et véhicule ses orientations en matière de gestion rationnelle des terres et des ressources du domaine de l’Etat. Le rôle du PNAT est d’adresser une orientation à chaque zone du territoire correspondant à une ou plusieurs activités, dans l’objectif <b>de mettre en valeur les ressources du territoire, de minimiser le risque de conflit entre les usages incompatibles, et d’optimiser les opportunités en termes d’usages multiples et compatibles.</b></p><p>La plateforme web interactive PNAT est l’outil de travail quotidien des personnes en charge de la mise en œuvre du PNAT au niveau décisionnel, politique et stratégique mais aussi au niveau technique. L’objectif est qu’elle soit un outil d’interaction entre les différents acteurs du PNAT quel que soit leur localisation géographique.</p>",
+        AppTitle: "Autres applications",
+        PartnerTitle: "Un partenariat entre",
+        Share: "Partager",
+        ContactUs: "Contactez-nous"   
+    }
+}
+
+var flag = {
+    href: "http://www.eaux-forets.gouv.ga/",
+    src: "img/flag/flag-gab.png",
+}
+
+var bcgImage = {
+    src: "url(img/title-bg.jpg) no-repeat center center scroll"
+}
+
+var link = {
+    en: {
+        mapLink: "http://ageos.maps.arcgis.com/apps/webappviewer/index.html?id=4ed40a5be5114b758cc88eb2f505cecc",
+        planLink: "http://ageos.maps.arcgis.com/apps/OnePane/storytelling_basic/index.html?appid=80a8f615fba740a9a0bfdc822637ca6b",
+        docsLink: "http://ageos.maps.arcgis.com/apps/MapJournal/index.html?appid=d3f4bf5903a545de9ebe20314cfb9332"
+
+    },
+    fr: {
+        mapLink: "http://ageos.maps.arcgis.com/apps/webappviewer/index.html?id=4ed40a5be5114b758cc88eb2f505cecc",
+        planLink: "http://ageos.maps.arcgis.com/apps/OnePane/storytelling_basic/index.html?appid=80a8f615fba740a9a0bfdc822637ca6b",
+        docsLink: "http://ageos.maps.arcgis.com/apps/MapJournal/index.html?appid=d3f4bf5903a545de9ebe20314cfb9332"
+    }
+}
+
+var apps = {
+    ageos: {
+        href: "http://globalforestwatch.org/",
+        src: "img/logo/gfw-logo-bw.png",
+        srcActive: "img/logo/gfw-logo.png",
+        alt: "GFW"
+    },
+    climat: {
+        href: "http://www.usaid.gov/",
+        src: "img/logo/gfw-logo-bw.png",
+        srcActive: "img/logo/gfw-logo.png",
+        alt: "USAID"
+    },
+    forestAtlas: {
+        href: "http://carpe.umd.edu/",
+        src: "img/logo/gfw-logo-bw.png",
+        srcActive: "img/logo/gfw-logo.png",
+        alt: "CARPE"
+    },
+    gfw: {
+        href: "http://globalforestwatch.org/",
+        src: "img/logo/gfw-logo-bw.png",
+        srcActive: "img/logo/gfw-logo.png",
+        alt: "GFW"
+    }
+}
+
+var partners = {
+    gabMin: {
+        href: "http://www.eaux-forets.gouv.ga/",
+        src: "img/logo/gab-logo.png",
+        alt: "Gabon Logo"
+    },
+    wri: {
+        href: "http://www.wri.org/",
+        src: "img/logo/wri-logo.png",
+        alt: "WRI Logo"
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,156 @@
+function iconHover() {
+	var image1 = document.getElementById("map-icon");
+	var image2 = document.getElementById("plan-icon");
+	var image3 = document.getElementById("docs-icon");
+
+	var link1 = document.getElementById("mapLink");
+	var link2 = document.getElementById("planLink");
+	var link3 = document.getElementById("docsLink");
+
+	if (!("ontouchstart" in document.documentElement) && 
+		! (navigator.maxTouchPoints > 0) &&
+		! (navigator.msMaxTouchPoints > 0) ){
+
+	link1.addEventListener("mouseover", function(){
+		image1.src="img/map-icon-active.svg";}, false);
+	link1.addEventListener("mouseout", function(){
+		image1.src="img/map-icon.svg";}, false);
+	link2.addEventListener("mouseover", function(){
+		image2.src="img/plan-icon-active.svg";}, false);
+	link2.addEventListener("mouseout", function(){
+		image2.src="img/plan-icon.svg";}, false);
+	link3.addEventListener("mouseover", function(){
+		image3.src="img/docs-icon-active.svg";}, false);
+	link3.addEventListener("mouseout", function(){
+		image3.src="img/docs-icon.svg";}, false);
+	};   
+};
+
+//Set language and update URL hash
+function langtoggle(l){
+
+	if (l == 'en' ){
+		local_lang = lang.en,
+		local_link = link.en
+		window.location.hash = "#l=en";
+	}
+
+	else if (l == 'fr') {
+		local_lang = lang.fr,
+		local_link = link.fr
+		window.location.hash = "#l=fr";
+	}	
+
+	for(var key in local_lang) {
+		document.getElementById(key).innerHTML = local_lang[key];
+	}
+	for(var key in local_link) {
+		document.getElementById(key).href = local_link[key];
+	}
+}
+
+//Check URL hash; set appropriate page language.
+function loadLanguage() {
+
+	var domain = window.location.origin.split(".")[1];
+	var hash = window.location.hash;
+
+	if (hash == '#l=en') {
+		langtoggle('en')
+	}
+	else {
+		langtoggle('fr')
+	}
+}
+
+//Generate flag link/image from country JS file
+function generateFlag() {
+
+	var flagContainer = document.getElementById('flag');
+
+	var newLink = document.createElement('a');
+	var newImg = document.createElement('img');
+
+	newLink.href = flag["href"];
+	newImg.setAttribute('src',flag["src"]);
+	newImg.className += " img-responsive";
+
+	newLink.appendChild(newImg);
+	flagContainer.appendChild(newLink);
+}
+
+//Generate background image from country JS file
+function generateBcgImg() {
+document.getElementById('intro').style.background = bcgImage["src"];
+}
+
+
+//Generate sponsor logos from country JS file
+function generateAppLogos() {
+
+	var appLogoContainer = document.getElementById('AppLogos');
+
+	for(var key in apps) 
+	{
+		var app = apps[key];
+		var newDiv = document.createElement('div');
+		var newLink = document.createElement('a');
+		var newImg = document.createElement('img');
+		newDiv.setAttribute('id',key);
+		newDiv.className += "col-xs-2" + " col-lg-3" + " supporter";
+		newLink.href = app["href"];
+		newLink.target = "_blank";
+		newImg.className += " img-responsive";
+		newImg.setAttribute('src',app["src"]);
+		newImg.setAttribute('alt',app["alt"]);
+		
+		newLink.appendChild(newImg);
+		newDiv.appendChild(newLink);
+		appLogoContainer.appendChild(newDiv);
+
+		//Trigger hover events
+		newImg.addEventListener("mouseover", function(){
+			var divSource = $(this).closest('div').attr('id');
+			this.setAttribute('src', app[divSource]["srcActive"])}, false);
+
+		newImg.addEventListener("mouseout", function(){
+			var divSource = $(this).closest('div').attr('id');
+			this.setAttribute('src', app[divSource]["src"])}, false);;
+}
+};
+
+
+//Generate partner logos from country JS file
+function generatePartnerLogos() {
+
+	var logoContainer = document.getElementById('PartnerLogos');
+
+	for(var key in partners) 
+	{
+		var partner = partners[key];
+		var newDiv = document.createElement('div');
+		var newLink = document.createElement('a');
+		var newImg = document.createElement('img');
+		newDiv.setAttribute('id',key);
+		newDiv.className += "col-xs-6" + " col-md-4" + " col-lg-3";
+		newLink.href = partner["href"];
+		newImg.className += " img-responsive";
+		newImg.setAttribute('src',partner["src"]);
+		newImg.setAttribute('alt',partner["alt"]);
+
+		newLink.appendChild(newImg);
+		newDiv.appendChild(newLink);
+		logoContainer.appendChild(newDiv);
+	}
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+	loadLanguage();
+	generateFlag();
+	generateBcgImg();
+	iconHover();
+	generateAppLogos();
+	generatePartnerLogos();
+}, false);
+
+SocialShareKit.init();
