12d4e57f5d6ba426b01067fbe2ffcaea8eddd088
Cooper Thomas <cooperthomascooper@gmail.com>
2015-11-25 19:19:58 -0500

    Added fourth contact to footer

--- js/content.js
+++ js/content.js
@@ -21,7 +21,9 @@
         ContactUs: "Contact us",
         Contact1: "<p>Ghislain Moussavou</p><p><i>Gabon National Coordinator, WRI</i></p><p><a href='mailto:gmoussavou@wri.org'>gmoussavou@wri.org</a></p>",
         Contact2: "<p>Matthew Steil</p><p><i>Manager, Central Africa Forests, WRI</i></p><p><a href='mailto:msteil@wri.org'>msteil@wri.org</a></p>",
-        Contact3: "<p>John Doe</p><p><i>Occupation</i></p><p><a href='mailto:email@email.com'>email@email.com</a></p>"
+        Contact3: "<p>John Doe</p><p><i>Occupation</i></p><p><a href='mailto:email@email.com'>email@email.com</a></p>",
+        Contact4: "<p>John Doe</p><p><i>Occupation</i></p><p><a href='mailto:email@email.com'>email@email.com</a></p>"
+
     },
     
     fr: {
@@ -46,7 +48,9 @@
         ContactUs: "Contactez-nous",
         Contact1: "<p>Ghislain Moussavou</p><p><i>Coordinateur National du Gabon, WRI</i></p><p><a href='mailto:gmoussavou@wri.org'>gmoussavou@wri.org</a></p>",
         Contact2: "<p>Matthew Steil</p><p><i>Gestionnaire, Forêts d'Afrique Centrale, WRI</i></p><p><a href='mailto:msteil@wri.org'>msteil@wri.org</a></p>",
-        Contact3: "<p>Jean Dupont</p><p><i>Occupation</i></p><p><a href='mailto:email@email.com'>email@email.com</a></p>"
+        Contact3: "<p>Jean Dupont</p><p><i>Occupation</i></p><p><a href='mailto:email@email.com'>email@email.com</a></p>",
+        Contact4: "<p>Jean Dupont</p><p><i>Occupation</i></p><p><a href='mailto:email@email.com'>email@email.com</a></p>"
+
     }
 }
 
