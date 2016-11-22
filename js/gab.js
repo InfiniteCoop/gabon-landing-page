bd36219f2728975333c7c61a6bdfb220d7d27175
Jospin Bleime <jojobound@yahoo.fr>
2016-11-22 13:30:46 +0100

    update coming soon links

--- js/gab.js
+++ js/gab.js
@@ -2,8 +2,8 @@
     en: {
         FrButton: "Français",
         EngButton: "English",
-        Title: "National Land Use Plan",
-        Subtitle: "The interactive platform",
+        Title: "National Land Use Plan of Gabon",
+        SubTitle: "The interactive platform",
         Module1: "Current land use",
         Module2: "Planning",
         Module3: "Documentation",
@@ -20,17 +20,14 @@
         Share: "Share this page",
         ContactUs: "Contact us",
         Contact1: "<p>Ghislain Moussavou</p><p><i>Gabon National Coordinator, WRI</i></p><p><a href='mailto:gmoussavou@wri.org'>gmoussavou@wri.org</a></p>",
-        Contact2: "<p>Matthew Steil</p><p><i>Manager, Central Africa Forests, WRI</i></p><p><a href='mailto:msteil@wri.org'>msteil@wri.org</a></p>",
-        Contact3: "<p>Judy MAMBELA</p><p><i>Occupation</i></p><p><a href='mailto:email@email.com'>email@email.com</a></p>",
-        Contact4: "<p>Jospin</p><p><i>Occupation</i></p><p><a href='mailto:email@email.com'>email@email.com</a></p>"
-
+        Contact2: "<p>Matthew Steil</p><p><i>Manager, Central Africa Forests, WRI</i></p><p><a href='mailto:msteil@wri.org'>msteil@wri.org</a></p>"
     },
     
     fr: {
         FrButton: "Français",
         EngButton: "English",        
-        Title: "Plan National d'Affectation des Terres",
-        Subtitle: "La plate-forme interactive",
+        Title: "Plan National d'Affectation des Terres du Gabon",
+        SubTitle: "La plate-forme interactive",
         Module1: "Affectation actuelle",
         Module2: "Planification",
         Module3: "Documentation",
@@ -40,31 +37,37 @@
         Caption2: "Explorer les analyses et scénarios d'affectation des terres potentielles, et réaliser des analyses personnalisées",
         CaptionTitle3: "Documentation",        
         Caption3: "Explorer les documents de référence sur le PNAT (programmes et projets gouvernementaux, plans sectoriels, cartothèque, etc.)",       
-        AboutTitle: "A Propos du PNAT",
+        AboutTitle: "A Propos le Plan",
         AboutText: "<p>Le PNAT est un processus de planification par lequel le gouvernement établit et véhicule ses orientations en matière de gestion rationnelle des terres et des ressources du domaine de l’Etat. Le rôle du PNAT est d’adresser une orientation à chaque zone du territoire correspondant à une ou plusieurs activités, dans l’objectif <b>de mettre en valeur les ressources du territoire, de minimiser le risque de conflit entre les usages incompatibles, et d’optimiser les opportunités en termes d’usages multiples et compatibles.</b></p><p>La plateforme web interactive PNAT est l’outil de travail quotidien des personnes en charge de la mise en œuvre du PNAT au niveau décisionnel, politique et stratégique mais aussi au niveau technique. L’objectif est qu’elle soit un outil d’interaction entre les différents acteurs du PNAT quel que soit leur localisation géographique.</p>",
         AppTitle: "Autres applications",
         PartnerTitle: "Un partenariat entre",
         Share: "Partager cette page",
         ContactUs: "Contactez-nous",
         Contact1: "<p>Ghislain Moussavou</p><p><i>Coordinateur National du Gabon, WRI</i></p><p><a href='mailto:gmoussavou@wri.org'>gmoussavou@wri.org</a></p>",
-        Contact2: "<p>Matthew Steil</p><p><i>Gestionnaire, Forêts d'Afrique Centrale, WRI</i></p><p><a href='mailto:msteil@wri.org'>msteil@wri.org</a></p>",
-        Contact3: "<p>Jean Dupont</p><p><i>Occupation</i></p><p><a href='mailto:email@email.com'>email@email.com</a></p>",
-        Contact4: "<p>Jean Dupont</p><p><i>Occupation</i></p><p><a href='mailto:email@email.com'>email@email.com</a></p>"
-
+        Contact2: "<p>Matthew Steil</p><p><i>Gestionnaire, Forêts d'Afrique Centrale, WRI</i></p><p><a href='mailto:msteil@wri.org'>msteil@wri.org</a></p>"
     }
+}
+
+var flag = {
+    href: "http://www.eaux-forets.gouv.ga/",
+    src: "img/flag/flag-gab.png",
+}
+
+var bcgImage = {
+    src: "url(img/title-bg.jpg) no-repeat center center scroll"
 }
 
 var link = {
     en: {
         mapLink: "http://ageos.maps.arcgis.com/apps/webappviewer/index.html?id=4ed40a5be5114b758cc88eb2f505cecc",
-        //planLink: "#",
-        //docsLink: "#"
+        planLink: "#",
+        docsLink: "#"
 
     },
     fr: {
         mapLink: "http://ageos.maps.arcgis.com/apps/webappviewer/index.html?id=4ed40a5be5114b758cc88eb2f505cecc",
-        //planLink: "#",
-        //docsLink: "#"
+        planLink: "#",
+        docsLink: "#"
     }
 }
 
@@ -76,7 +79,7 @@ var apps = {
         alt: "AGEOS"
     },
     climat: {
-        href: "http://www.conseilnationalclimat.ga",
+        href: "http://www.gouvernement.ga/",
         src: "img/logo/climat-logo-bw.png",
         srcActive: "img/logo/climat-logo.png",
         alt: "Climat"
@@ -87,12 +90,6 @@ var apps = {
         srcActive: "img/logo/forestatlas-logo.png",
         alt: "Atlas Forestier"
     },
-	siap: {
-        href: "http://anpnsig.ga/",
-        src: "img/logo/forestatlas-logo-bw.png",
-        srcActive: "img/logo/forestatlas-logo.png",
-        alt: "SIAP ANPN"
-	},
     gfw: {
         href: "http://globalforestwatch.org/",
         src: "img/logo/gfw-logo-bw.png",
